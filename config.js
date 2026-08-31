40067cf6d089325ec3436a0c680878080c74f03b
maxzalaquett-maker <maxzalaquett@gmail.com>
2026-08-31 16:44:13 -0400

    Load directory filter icon enhancements

--- config.js
+++ config.js
@@ -33,6 +33,8 @@ loadStyle('/foster-categories.css');
 loadScript('/foster-categories.js');
 loadStyle('/phase1.css');
 loadScript('/phase1.js');
+loadStyle('/filter-icons.css');
+loadScript('/filter-icons.js');
 loadStyle('/logos.css');
 loadScript('/logos.js');
 
